411307d387f1c7051d0f6fcd0cf2b68c0319d52a
zhttower <453522183@qq.com>
2017-08-15 13:48:55 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <vheader></vheader>
+    <div class="tab"></div>
+    <div class="content"></div>
+  </div>
+</template>
+
+<script>
+  import vheader from './components/header/header';
+
+  export default {
+    components: {
+      vheader
+    }
+  };
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>I am header</div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style>
+
+</style>
