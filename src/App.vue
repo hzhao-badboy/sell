a454fe617c519061478ac5d80434f1b18e2776f4
badboy <453522183@qq.com>
2017-08-17 23:08:37 +0800

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <vheader></vheader>
-    <div class="tab"></div>
-    <div class="content"></div>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link></div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link></div>
+      <div class="tab-item">
+        <router-link to="/sellers">商家</router-link></div>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -16,6 +23,13 @@
   };
 </script>
 
-<style>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    .tab-item
+      flex:1
+      text-align: center
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import VueRouter from 'vue-router';
+import goods from './components/goods/goods';
+
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
+
+var routs = [
+  {path: '/goods', component: goods}
+];
+
+var router = new VueRouter({
+  routs
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   template: '<App/>',
   components: { App }
 });
+
